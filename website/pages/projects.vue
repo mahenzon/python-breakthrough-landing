c376126faa95e90c9f6af1f3896c313bf7dcaf7e
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-10-06 16:12:36 +0300

    remove redundant header

--- website/pages/projects.vue
+++ website/pages/projects.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container mx-auto px-4 py-12">
-    <h1 class="text-4xl font-bold mb-8">Проекты</h1>
     
     <!-- Course Projects Section -->
     <section class="mb-16">
